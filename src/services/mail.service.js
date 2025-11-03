10e9f9d1cf09ab0d982e77319f63b2892a678ed8
Jayant-Thakre-05 <jayantthakre@gmail.com>
2025-11-03 12:48:19 +0530

    last

--- src/services/mail.service.js
+++ src/services/mail.service.js
@@ -3,8 +3,8 @@ const nodemailer = require("nodemailer")
 const transporter = nodemailer.createTransport({
     service:"gmail",
     auth:{
-        user:"jayantthakre70@gmail.com",
-        pass:"qrvffnmztgesonwj",
+        user:process.env.TRANSPORTER_EMAIL,
+        pass:process.env.TRANSPORTER_PASSWORD,
     }
 })
 
